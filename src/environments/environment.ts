22591e506bcf2734eeabb70f9d0bf33dcc7f7ad7
franco <fr.toledop@duocuc.cl>
2023-07-30 15:22:14 -0400

    Respaldo

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'mastergym-d2024',
+    appId: '1:481645718529:web:791669fb104831ac256400',
+    databaseURL: 'https://mastergym-d2024-default-rtdb.firebaseio.com',
+    storageBucket: 'mastergym-d2024.appspot.com',
+    apiKey: 'AIzaSyDurS4d-WOlvXO6sMA1Kzrj05mSFZekips',
+    authDomain: 'mastergym-d2024.firebaseapp.com',
+    messagingSenderId: '481645718529',
+    measurementId: 'G-FLRMHXTNGM',
+  },
+};
